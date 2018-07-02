27b275dec3e0bb8466ceb0997dc6bc98c880e972
Andrew Allen <andrew@allenan.com>
2018-07-02 16:29:41 -0700

    fix the release (#1)

--- mix.exs
+++ mix.exs
@@ -16,15 +16,16 @@ defmodule BlockchainNode.MixProject do
     [
       extra_applications: [:logger, :gpb, :intercept, :rand_compat],
       mod: {BlockchainNode.Application, []},
-      env: [port: 0]
+      env: [port: 0],
+      included_applications: [:blockchain]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:distillery, github: "bitwalker/distillery", branch: "master", runtime: false},
-      {:blockchain, git: "git@github.com:helium/blockchain.git", branch: "rg/payment-txn", runtime: false},
+      {:distillery, "~> 1.5", runtime: false},
+      {:blockchain, git: "git@github.com:helium/blockchain.git", branch: "rg/payment-txn"},
       {:bitcask, git: "git@github.com:helium/bitcask.git", branch: "otp21", override: true},
       {:lager, ~r/.*/, env: :prod, git: "https://github.com/erlang-lager/lager.git", branch: "adt/sys-trace-func", manager: :rebar3, override: true},
       {:cowboy, "~> 1.0.0"},
