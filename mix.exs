8ecc3bae28c5dadd968967293c2d13e0570d7ae5
Andrew Allen <allenan@helium.com>
2018-07-02 16:30:15 -0700

    use forked version of distillery 1.5.x with spaces branch

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule BlockchainNode.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:distillery, "~> 1.5", runtime: false},
+      {:distillery, github: "allenan/distillery", branch: "spaces", runtime: false},
+      #{:distillery, "~> 1.5", runtime: false},
       {:blockchain, git: "git@github.com:helium/blockchain.git", branch: "rg/payment-txn"},
       {:bitcask, git: "git@github.com:helium/bitcask.git", branch: "otp21", override: true},
       {:lager, ~r/.*/, env: :prod, git: "https://github.com/erlang-lager/lager.git", branch: "adt/sys-trace-func", manager: :rebar3, override: true},
