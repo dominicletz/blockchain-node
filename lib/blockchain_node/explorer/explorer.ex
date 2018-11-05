5114e2b4d3cd59b7abc9a504b9aba12b0f626651
Andrew Allen <allenan@helium.com>
2018-11-05 12:04:57 -0800

    handle unknown txns

--- lib/blockchain_node/explorer/explorer.ex
+++ lib/blockchain_node/explorer/explorer.ex
@@ -14,7 +14,7 @@ defmodule BlockchainNode.Explorer do
             nonce: nonce,
             transactions:
               all_transactions
-              |> Enum.filter(fn txn -> txn.payer == address or txn.payee == address end)
+              |> Enum.filter(fn txn -> txn[:payer] == address or txn[:payee] == address end)
           }
         end
     end
@@ -66,6 +66,18 @@ defmodule BlockchainNode.Explorer do
     }
     |> Map.merge(parse_txn_block(block_hash, block))
   end
+
+  # defp parse_txn(block_hash, block, {:txn_create_htlc, payer, payee, amount, fee, nonce, sig}) do
+  #   %{
+  #     type: "payment",
+  #     payer: payer |> :libp2p_crypto.address_to_b58() |> to_string(),
+  #     payee: payee |> :libp2p_crypto.address_to_b58() |> to_string(),
+  #     amount: amount,
+  #     fee: fee,
+  #     nonce: nonce
+  #   }
+  #   |> Map.merge(parse_txn_block(block_hash, block))
+  # end
 
   defp parse_txn(block_hash, block, _unknown_txn) do
     %{
