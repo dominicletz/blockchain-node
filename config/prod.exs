d9b0d8a885c01f2648f6b18ac32ffc37b9a8eb1f
Amir Haleem <hakeem@gmail.com>
2018-11-08 10:33:16 -0800

    fix seed nodes

--- config/prod.exs
+++ config/prod.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
 config :blockchain,
-  seed_nodes: "/ip4/34.222.64.221/tcp/2154,/ip4/34.208.255.251/tcp/2154"
+  seed_nodes: ["/ip4/34.222.64.221/tcp/2154", "/ip4/34.208.255.251/tcp/2154"]
